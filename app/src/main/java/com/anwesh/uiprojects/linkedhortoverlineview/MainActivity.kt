1046aa5a1ee7b582caa79f06925de59a9e42ed55
Anwesh43 <anweshthecool0@gmail.com>
2018-09-19 11:03:59 +0530

    adding viriew in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedhortoverlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedhortoverlineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedhortoverlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.hortoverlineview.HorToVerLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        HorToVerLineView.create(this)
     }
 }
