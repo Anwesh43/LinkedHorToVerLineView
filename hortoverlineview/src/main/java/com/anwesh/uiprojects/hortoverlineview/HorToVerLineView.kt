91ed4949f9fb55495cf480eec48a4485dcd77147
Anwesh43 <anweshthecool0@gmail.com>
2018-09-19 10:18:29 +0530

    adding libraries and defining number of nodes

--- hortoverlineview/src/main/java/com/anwesh/uiprojects/hortoverlineview/HorToVerLineView.kt
+++ hortoverlineview/src/main/java/com/anwesh/uiprojects/hortoverlineview/HorToVerLineView.kt
@@ -3,3 +3,11 @@ package com.anwesh.uiprojects.hortoverlineview
 /**
  * Created by anweshmishra on 19/09/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+
+val nodes : Int = 5
