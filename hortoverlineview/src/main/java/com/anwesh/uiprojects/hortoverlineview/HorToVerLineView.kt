13f79e391ddc4bc878ed87f49927f0f3fe1f2996
Anwesh43 <anweshthecool0@gmail.com>
2018-09-19 11:04:09 +0530

    adding drawing fixes

--- hortoverlineview/src/main/java/com/anwesh/uiprojects/hortoverlineview/HorToVerLineView.kt
+++ hortoverlineview/src/main/java/com/anwesh/uiprojects/hortoverlineview/HorToVerLineView.kt
@@ -29,7 +29,7 @@ fun Canvas.drawHTVNode(i : Int, scale : Float, paint : Paint) {
     translate(gap * i + gap, h/2)
     for (j in 0..1) {
         val sf : Float = 1f - 2 * j
-        val sc : Float = Math.min(0.5f, Math.max(0f, scale - j * 0.5f))
+        val sc : Float = Math.min(0.5f, Math.max(0f, scale - j * 0.5f)) * 2
         save()
         rotate(90f * sc)
         drawLine(0f, 0f, (gap/2) * sf, 0f, paint)
@@ -60,7 +60,7 @@ class HorToVerLineView(ctx : Context) : View(ctx) {
     data class State(var scale : Float = 0f, var prevScale : Float = 0f, var dir : Float = 0f) {
 
         fun update(cb : (Float) -> Unit) {
-            scale += 0.1f * this.dir
+            scale += 0.05f * this.dir
             if (Math.abs(scale - prevScale) > 1) {
                 scale = prevScale + dir
                 dir = 0f
@@ -108,6 +108,10 @@ class HorToVerLineView(ctx : Context) : View(ctx) {
     data class HTVNode(var i : Int, val state : State = State()) {
         private var next : HTVNode? = null
         private var prev : HTVNode? = null
+
+        init {
+            addNeighbor()
+        }
 
         fun addNeighbor() {
             if (i < nodes - 1) {
@@ -191,8 +195,8 @@ class HorToVerLineView(ctx : Context) : View(ctx) {
     }
 
     companion object {
-        private lateinit var view : HorToVerLineView
-        fun create(activity : Activity) : HorToVerLineView {
+        private var view : HorToVerLineView? = null
+        fun create(activity : Activity) : HorToVerLineView? {
             if (view == null) {
                 view = HorToVerLineView(activity)
                 activity.setContentView(view)
